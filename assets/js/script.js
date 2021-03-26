c34da8902304c1ce2d626eded2819244adf58175
Jin Hong <jnhg1016@gmail.com>
2021-03-27 08:47:16 +1100

    added check local storage and preset dat to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
     //1. Create html of the timeblocks dynamically
-    
     for (var i = 9; i <= 17; i++){
         $('.container').append(`<div class="row time-block" data-time="${i}"> 
         <div class="col-sm col-md-2 hour"> 
@@ -15,3 +14,21 @@ $(document).ready(function() {
         </div> 
         </div>`);
     }
+       
+    var timeTrackObject = {};
+        //2. Checks if local storage exists, if it doesn't load preset data to array.
+        if (localStorage.getItem('timeTrackObject')) {
+            timeTrackObject = JSON.parse(localStorage.getItem('timeTrackObject'));
+        }else{
+            timeTrackObject = {
+                '9am': { time: "9am", value: ""},
+                '10am':{ time: "10am", value: ""},
+                '11am':{ time: "11am", value: ""},
+                '12pm':{ time: "12pm", value: ""},
+                '1pm':{ time: "1pm", value: ""},
+                '2pm':{ time: "2pm", value: ""},
+                '3pm':{ time: "3pm", value: ""},
+                '4pm':{ time: "4pm", value: ""},
+                '5pm':{ time: "5pm", value: ""}
+            };
+        }
