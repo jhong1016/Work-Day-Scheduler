dac6fb1ebf7e58c28b1005536ef3715c88a00944
Jin Hong <jnhg1016@gmail.com>
2021-03-27 10:29:47 +1100

    time slots added

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,41 @@ function init(now=moment()) {
     dateKey = now.format('YYYYMMDD')
     if (!(dateKey in values)) values[dateKey] = {}
 
+    // Build the time slots
+    var container = $('.container')
+    for (let hr = business.start; hr < business.end; hr++) {
+        let time = moment(hr, 'H')
+        let frame = hour > hr ? 'past' : hour < hr ? 'future' : 'present'
+        var eventText = values[dateKey][hr] || ''
+        let row = $(`
+        <div id='time-slot-${hr}' class='time-block row'>
+            <span class='hour time-column col-1'>
+                <span class='hour-display'>${time.format('hA')}</span>
+            </span>
+            <span class='info-column col ${frame}'>
+                <textarea id='event-input-${hr}' data-hr=${hr} type="text" class='event-input'>${eventText}</textarea>
+            </span>
+            <span id='save-button-${hr}' data-hr=${hr} class='saveBtn col-1'>
+            </span>
+        </div>`)
+        container.append(row)
+    }
+    container.on('click', event => {
+        if (event.target.matches('.saveBtn')) {
+            var button = event.target
+            var hr = button.dataset.hr
+            var inputValue = $(`#event-input-${hr}`)[0].value
+            values[dateKey][hr] = inputValue
+            localStorage.setItem('schedulerData', JSON.stringify(values))
+        }
+    })
+
+    localStorage.setItem('schedulerData', JSON.stringify(values))
+
+    setAlarm(now)
+}
+init()
+
 
 
 $("#clearData").click(function() {
